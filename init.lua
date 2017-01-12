b54828f771f5b357538cc95ac1bb1bf353289be4
Baptiste Pouget <baba@firemail.cc>
2017-01-12 17:19:24 +0100

    added straw bed

---
+++ init.lua
@@ -0,0 +1,18 @@
+beds.register_bed("straw_bed:straw_bed",{
+    description = "Straw bed",
+    inventory_image = "straw_bed.png",
+    wield_image = "straw_bed.png",
+    nodebox = {
+        bottom = {-0.5,-0.5,-0.5,0.5,-0.49,0.5},
+        top = {-0.5,-0.5,-0.5,0.5,-0.49,0.5}
+    },
+    selectionbox = {-0.5,-0.5,-0.5,0.5,-0.49,1.5},
+    tiles = {
+        bottom = {"straw_bed_bottom.png"},
+        top = {"straw_bed_top.png"}
+    },
+    recipe = {
+        {"group:grass","group:grass","group:grass"},
+        {"group:grass","group:grass","group:grass"}
+    }
+})
