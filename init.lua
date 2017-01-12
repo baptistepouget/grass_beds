1122340ed01cb3b3cd0522520f7a285e25e666b6
Baptiste Pouget <baba@firemail.cc>
2017-01-12 18:24:36 +0100

    Add transparent sides

--- init.lua
+++ init.lua
@@ -2,14 +2,29 @@ beds.register_bed("straw_bed:straw_bed",{
     description = "Straw bed",
     inventory_image = "straw_bed.png",
     wield_image = "straw_bed.png",
+    sounds = default.node_sound_leaves_defaults(),
     nodebox = {
         bottom = {-0.5,-0.5,-0.5,0.5,-0.49,0.5},
         top = {-0.5,-0.5,-0.5,0.5,-0.49,0.5}
     },
     selectionbox = {-0.5,-0.5,-0.5,0.5,-0.49,1.5},
     tiles = {
-        bottom = {"straw_bed_bottom.png"},
-        top = {"straw_bed_top.png"}
+        bottom = {
+            "straw_bed_bottom.png",
+            "straw_bed_bottom.png",
+            "straw_bed_side.png",
+            "straw_bed_side.png",
+            "straw_bed_side.png",
+            "straw_bed_side.png"
+        },
+        top = {
+            "straw_bed_top.png",
+            "straw_bed_top.png",
+            "straw_bed_side.png",
+            "straw_bed_side.png",
+            "straw_bed_side.png",
+            "straw_bed_side.png"
+        }
     },
     recipe = {
         {"group:grass","group:grass","group:grass"},
